e91f1ecfa7376e159149c51e9017716cb1126e52
Kevin Anantha <kevin.anantha@gmail.com>
2018-07-09 16:46:30 +0800

    Init

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,20 @@ def create
   end
 end
 
+def update
+  @post = Post.find(params[:id])
+
+  if @post.update(post_params)
+    redirect_to @post
+  else
+    render 'edit'
+  end
+end
+
+def edit
+  @post = Post.find(params[:id])
+end
+
 def show
   @post = Post.find(params[:id])
 end
