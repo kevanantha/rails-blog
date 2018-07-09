84733caec4fde5b37834e2e2cafe75a5a99b6dd8
Kevin Anantha <kevin.anantha@gmail.com>
2018-07-09 19:53:15 +0800

    Init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_08_171427) do
+ActiveRecord::Schema.define(version: 2018_07_09_115034) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
